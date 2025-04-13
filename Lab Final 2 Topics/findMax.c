4a9e3e481ec2b1e89ffc1c53fb5ca1630ce69e86
Mahidul Haque <161691748+mahidulhq@users.noreply.github.com>
2025-04-13 09:14:25 +0600

    Update findMax.c

--- Lab Final 2 Topics/findMax.c
+++ Lab Final 2 Topics/findMax.c
@@ -1,20 +1,18 @@
-// Write a function to find the maximum of three numbers.
-#include<stdio.h>
-int fmax(int a, int b, int c) {
-    if (a >= b && a >= c ) {
+// Write a function to find the maximum of three numbers
+#include <stdio.h>
+int findMax(int a, int b, int c) {
+    if(a >= b && a >= c) {
         return a;
-    } else if (b >= a && b >= c) {
+    } else if( b >= a && b >= c) {
         return b;
     } else {
         return c;
     }
 }
-
 int main() {
-    int a, b, c, m;
+    int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    m = fmax(a, b, c);
-    printf("%d\n", m);
+    printf("%d\n", findMax(a, b, c));
     return 0;
 }
